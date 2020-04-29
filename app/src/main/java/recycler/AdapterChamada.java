38c61a131a140f79e647500a5945a54bf3c367c2
kayel <kayel.yuri@gmail.com>
2020-04-28 22:35:09 -0300

    Recycler Click

--- app/src/main/java/recycler/AdapterChamada.java
+++ app/src/main/java/recycler/AdapterChamada.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.exemplo.R;
 
@@ -12,7 +13,7 @@ public class AdapterChamada extends RecyclerView.Adapter<ChamadaViewHolder> {
 
     private String[] chamada;
 
-    public AdapterChamada(String[] chamada){
+    public AdapterChamada(String[] chamada) {
         this.chamada = chamada;
     }
 
@@ -26,7 +27,14 @@ public class AdapterChamada extends RecyclerView.Adapter<ChamadaViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ChamadaViewHolder chamadaViewHolder, int position) {
-        chamadaViewHolder.textViewTexticulo.setText(chamada[position]);
+        final String nome = chamada[position];
+        chamadaViewHolder.textViewTexticulo.setText(nome);
+        chamadaViewHolder.textViewTexticulo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), nome, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
