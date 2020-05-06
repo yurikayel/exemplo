44e49ca3713d38005202825331f4ab1b48232504
kayel <kayel.yuri@gmail.com>
2020-05-06 17:04:36 -0300

    setupPagerAdapter

--- app/src/main/java/custom/CustomPagerAdapter.kt
+++ app/src/main/java/custom/CustomPagerAdapter.kt
@@ -6,25 +6,39 @@ import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 
-fun ViewPager.setupPagerAdapter(collection: Any, titles: List<String>) {
-    adapter = CustomPagerAdapter(collection, titles, (context as AppCompatActivity).supportFragmentManager)
+fun ViewPager.setupPagerAdapter(frags: Any, titles: Any) {
+    adapter = CustomPagerAdapter(
+        frags, titles, (context as AppCompatActivity).supportFragmentManager
+    )
 }
 
 class CustomPagerAdapter(
-    private val collection: Any,
-    private val titles: List<String>,
+    private val frags: Any,
+    private val titles: Any,
     fragmentManager: FragmentManager
 ) : FragmentPagerAdapter(fragmentManager) {
 
     @Suppress("UNCHECKED_CAST")
-    override fun getItem(position: Int) = when (collection) {
-        is List<*> -> collection[position] as Fragment
-        is Array<*> -> collection[position] as Fragment
+    override fun getItem(position: Int) = when (frags) {
+        is List<*> -> frags[position] as Fragment
+        is Array<*> -> frags[position] as Fragment
         else -> null
     }
 
-    override fun getPageTitle(position: Int) = titles[position]
+    override fun getPageTitle(position: Int) = when (titles) {
+        is List<*> -> titles[position] as String
+        is Array<*> -> titles[position] as String
+        else -> null
+    }
 
-    override fun getCount() = titles.size
+    override fun getCount() = when (frags) {
+        is List<*> -> frags.size
+        is Array<*> -> frags.size
+        else -> when (titles) {
+            is List<*> -> titles.size
+            is Array<*> -> titles.size
+            else -> 2
+        }
+    }
 }
 
